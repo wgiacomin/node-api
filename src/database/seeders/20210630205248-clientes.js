5314adc7ae7a230a845273d84b08eeed2906ac25
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2021-07-04 17:58:09 -0300

    correcao seed associado

--- src/database/seeders/20210630205248-clientes.js
+++ src/database/seeders/20210630205248-clientes.js
@@ -2,25 +2,31 @@
 
 module.exports = {
   up: async (queryInterface) => {
+
+    const associados = await queryInterface.sequelize.query(
+      'SELECT id FROM Associados'
+    )
+    const associadosRows = associados[0]
+
     return queryInterface.bulkInsert(
       'Clientes', [
         {
           nome: 'Don Don Yakisoba',
           cnpj: '34.600.167/0001-05',
           endereco: '300 Muller Plain Apt. 248 - North Miami, NJ / 22289',
-          associado: 1
+          associado: associadosRows[0]
         },
         {
           nome: 'Cho Street Food',
           cnpj: '10.834.715/0001-01',
           endereco: '48246 Brakus Forks Apt. 577 - Charleston, VA / 34232',
-          associado: 2
+          associado: associadosRows[1]
         },
         {
           nome: 'Matteo Burgers',
           cnpj: '67.063.214/0001-88',
           endereco: '47641 Natalia Fields Apt. 853 - Gresham, MT / 64494',
-          associado: 3
+          associado: associadosRows[2]
         },
       ]
     )
