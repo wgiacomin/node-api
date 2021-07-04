004584b6d87c4052aadab058a2f78e08567f8ac4
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2021-07-04 16:13:03 -0300

    format

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken')
 
-function generateToken(id, type){
-    process.env.JWT_SECRET = Math.random().toString(36).slice(-20)
-    const token = jwt.sign({ id, type }, process.env.JWT_SECRET, {
-        expiresIn: 82800,
-    })
-    return token
+function generateToken(id, type) {
+  process.env.JWT_SECRET = Math.random().toString(36).slice(-20)
+  const token = jwt.sign({ id, type }, process.env.JWT_SECRET, {
+    expiresIn: 82800,
+  })
+  return token
 }
 
 module.exports = generateToken
