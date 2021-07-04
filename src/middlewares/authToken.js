79cc456018519b262224d74e20f7c91fe5d6078a
Wanderson Giacomin <30360664+wgiacomin@users.noreply.github.com>
2021-07-04 16:14:05 -0300

    format auth

--- src/middlewares/authToken.js
+++ src/middlewares/authToken.js
@@ -2,22 +2,22 @@ const jwt = require('jsonwebtoken')
 const { MSG } = require('../utils/fieldsAndMsgs')
 
 function verifyToken(req, res, next){
-    const token = req.headers['x-access-token']
+  const token = req.headers['x-access-token']
     
-    if (!token){
-        return res.status(401).json({msg: MSG.TOKEN_AUSENTE})
+  if (!token){
+    return res.status(401).json({msg: MSG.TOKEN_AUSENTE})
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err){
+      return res.status(401).json({msg: MSG.TOKEN_INVALIDO})
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err){
-            return res.status(401).json({msg: MSG.TOKEN_INVALIDO})
-        }
-
-        req.user = {}
-        req.user.id = decoded.id
-        req.user.type = decoded.type
-        next()
-    })
+    req.user = {}
+    req.user.id = decoded.id
+    req.user.type = decoded.type
+    next()
+  })
 }
 
 module.exports = verifyToken
